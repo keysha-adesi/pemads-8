59d6534583868f16252227d9c476438370aabb46
keysha-adesi <keyshaputrii590@gmail.com>
2026-04-21 17:34:32 +0700

    nim

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -1,1 +1,16 @@
 #include <iostream>
+using namespace std;
+
+class mahasiswa{ // blue print atau cetakan 
+    public:
+    int nim;
+    string nama;
+    float niali;
+
+    void prinData(){
+        cout << "NIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        
+    }
+
+}
